d0f62b5fa47cf1d0bb848756b1bf0f577e185f41
dhruv1206 <86611328+dhruv1206@users.noreply.github.com>
2025-05-09 22:07:36 +0530

    Refactor memory management to ensure thread-safe operations

--- src/server.cpp
+++ src/server.cpp
@@ -38,12 +38,6 @@ void *operator new(size_t bytes)
     return malloc(bytes);
 }
 
-void operator delete(void *memory) noexcept
-{
-    MemoryManager::currentUsageBytes -= sizeof(memory);
-    free(memory);
-}
-
 void operator delete(void *memory, size_t bytes) noexcept
 {
     MemoryManager::currentUsageBytes -= bytes;
